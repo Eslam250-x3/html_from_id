94d512959a6e3ad9c4fbfb8ee677106e56098223
Eslam Mohamed <eslam.mohamed@nagwa.com>
2026-02-17 10:02:19 +0200

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: '/html_from_id/',
     plugins: [react()],
     server: {
         proxy: {
